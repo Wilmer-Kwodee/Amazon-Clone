6fb279d73a6ffadff6649c4949b2ff0798edc058
Wilmer Kwodee <152074108+Wilmer-Kwodee@users.noreply.github.com>
2024-02-17 10:04:44 +0700

    Add files via upload

---
+++ originalApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='p-5'>
+    This is a Website!
+    </div>
+  )
+}
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBR5IQFG1bDWesxlj3Wbt4z3cE0qAdaT1w",
+    authDomain: "challenge-91ace.firebaseapp.com",
+    projectId: "challenge-91ace",
+    storageBucket: "challenge-91ace.appspot.com",
+    messagingSenderId: "618573007480",
+    appId: "1:618573007480:web:debe8aad56c5091959bb8d"
+  };
