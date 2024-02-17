ffcb3556c965ba43cc599455d15de9d32f053a19
Wilmer Kwodee <152074108+Wilmer-Kwodee@users.noreply.github.com>
2024-02-17 12:15:04 +0700

    Add files via upload

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,14 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className='header'>
-        <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+        <Link to="/">
+          <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+        </Link>
         
         <div className='header__search'>
           <input className='header__searchInput' type='text' />
@@ -41,10 +44,12 @@ function Header() {
             </span>
           </div>
 
-          <div className='header__optionBasket'>
-            <ShoppingBasketIcon></ShoppingBasketIcon>
-            <span className='header__optionLineTwo header_basketCount'>0</span>
-          </div>
+          <Link to="/checkout">
+            <div className='header__optionBasket'>
+              <ShoppingBasketIcon></ShoppingBasketIcon>
+              <span className='header__optionLineTwo header_basketCount'>0</span>
+            </div>
+          </Link>
         </div>
     </div>
   )
---
+++ src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Subtotal.css'
+
+function Subtotal() {
+  return (
+    <div>
+        <h2>The subtotal will be </h2>
+    </div>
+  )
+}
+
+export default Subtotal
